94d47253a0723009f3f95e7adc5c3d03dc358d30
Ellen Gummesson <ellengummesson@gmail.com>
2013-09-13 20:35:23 +0200

    Add 'focuswriter'.

---
+++ focuswriter/tools/chocolateyInstall.ps1
@@ -0,0 +1,19 @@
+$Package = "FocusWriter"
+$Version = "1.4.4"
+
+$ToolsPath = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$PackagePath = "$((Get-Item $ToolsPath).parent.FullName)"
+
+$PackageParams = @{
+  packageName = $Package;
+  url = "http://gottcode.org/focuswriter/FocusWriter_$Version.zip";
+  unzipLocation = (Join-Path $PackagePath "bin");
+}
+
+Try {
+  Install-ChocolateyZipPackage @PackageParams
+  Write-ChocolateySuccess $Package
+} Catch {
+  Write-ChocolateyFailure $Package "$($_.Exception.Message)"
+  Throw
+}
